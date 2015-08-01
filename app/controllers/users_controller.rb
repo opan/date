fe4749adafe87f66557f5cdf12a1e6b576f0efa9
pwz2k <pwz2k@hotmail.com>
2015-08-01 11:53:27 -0400

    removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,11 +77,10 @@ end
 
   def show
     @user = User.find_by(username: params[:id])
-    log params.inspect, @user.inspect
-    @question = @user.questions.where("answer is not null").order("created_at DESC").page(params[:page]).per_page(3)
-    @letsgos = @user.letsgos.paginate(page: params[:page], :per_page => 3)
+    @question = user.questions.where("answer is not null").order("created_at DESC").page(params[:page]).per_page(3)
+    @letsgos = user.letsgos.paginate(page: params[:page], :per_page => 3)
     @letsgo = current_user.letsgos.build
-    @similar_users = @user.similar.shuffle.first(8)
+    @similar_users = user.similar.shuffle.first(8)
     respond_to do |format|
       format.html { render layout: 'new_application' }
       format.js { render partial: '/letsgos/letsgo_paging', locals: {letsgos: @letsgos} } if params[:letsgo]
