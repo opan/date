c93969eaa492d558565b91a65cc2444b33089603
pwz2k <pwz2k@hotmail.com>
2015-08-13 21:12:52 -0400

    seewhatitdoes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,18 @@ Dating::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.default_url_options = { :host => 'areyoutaken.com' }
+   config.action_mailer.smtp_settings = {
+     address:              'crikey.lunarbreeze.com',
+     port:                 465,
+     domain:               'areyoutaken.com',
+     user_name:            'yo@areyoutaken.com',
+     password:             'series2k',
+     authentication:       'plain',
+     enable_starttls_auto: false  }
+
+    config.action_mailer.perform_deliveries = true
     config.action_mailer.default_url_options = { :host => "areyoutaken.com" }
     
 end
